d669ae43d231fc587baa5fabee7ae86289cadbad
mitishraina <163857087+mitishraina@users.noreply.github.com>
2024-10-17 21:57:21 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,136 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Smart Home IoT',
+      debugShowCheckedModeBanner: false,
+      title: 'Dadu Care',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: SplashScreen(),
-      routes: {
-        '/home': (context) => HomePage(),
-        '/chat': (context) => ChatScreen(),
-        '/information': (context) => InformationScreen(),
-        '/voice': (context) => VoiceAssistanceScreen(),
-      },
-    );
-  }
-}
-
-// SplashScreen Widget
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, '/home');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Welcome to दादू Care',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-}
-
-// HomePage Widget
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/information');
-              },
-              child: Text('Information'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/chat');
-              },
-              child: Text('Community Chat'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/voice');
-              },
-              child: Text('Voice Assistance'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// ChatScreen Widget
-class ChatScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Community Chat'),
-      ),
-      body: Center(
-        child: Text(
-          'Chat with others here!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
-
-// InformationScreen Widget
-class InformationScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Information Page'),
-      ),
-      body: Center(
-        child: Text(
-          'User Information and Activity Logs',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
-
-// VoiceAssistanceScreen Widget
-class VoiceAssistanceScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Voice Assistance'),
-      ),
-      body: Center(
-        child: Text(
-          'Voice assistance functionality',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
     );
   }
 }
